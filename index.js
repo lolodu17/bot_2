adef00022fc2745512664f00a6fd6749815a3171
BeedyWool <35271362+BeedyWool@users.noreply.github.com>
2018-01-10 17:18:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -76,5 +76,25 @@ bot.on("message", msg => {
   }
 });
 
+bot.on("message", msg => {
+    if(message.content === "/purge") {
+        let messagecount = parseInt(args[1]) || 1;
 
+        var deletedMessages = -1;
+
+        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+            messages.forEach(m => {
+                if (m.author.id == bot.user.id) {
+                    m.delete().catch(console.error);
+                    deletedMessages++;
+                }
+            });
+        }).then(() => {
+                if (deletedMessages === -1) deletedMessages = 0;
+                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                    .then(m => m.delete(2000));
+        }).catch(console.error);
+    }
+});
+	  
 bot.login(process.env.TOKEN)
