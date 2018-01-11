6741a33eef898baf71f6d1830b8a6131f4812c5e
BeedyWool <35271362+BeedyWool@users.noreply.github.com>
2018-01-11 19:14:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ bot.on("ready", function () {
 	console.log("Bot connecté");
 });
 
+bot.on('message', message => {
+   if (message.content.startsWith("/piece")) {
+var commande = [":moneybag: | La pièce dit : Face.", ":moneybag: | La pièce dit : Pile."]
+     message.channel.send(`${(commande[Math.floor(Math.random() * commande.length)])}`)
+}
+
+});
+
 
 bot.on('message', msg => {
   if (msg.content.startsWith('/invite')) {
