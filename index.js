eb37e889f07273ff46b6a37f0637f37150b4ce9a
BeedyWool <35271362+BeedyWool@users.noreply.github.com>
2018-01-16 18:58:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -193,7 +193,9 @@ bot.on('message', message => {
       message.guild.channels.find("name", 'betaverse').send(message.content.slice(5, message.content.length));
    }
 });	
+});
 
+	
 bot.on('message', message => {
    if (message.content.startsWith("/bvn")) {
       message.delete(1000);
